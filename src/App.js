53d33384fdb8bd7ba1e320d5fa8393fc847b9bf1
Ethan Nguyen <nguyeeth@tamu.edu>
2023-12-29 18:18:06 -0800

    Start models and components

--- src/App.js
+++ src/App.js
@@ -1,9 +1,21 @@
 import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import 'bootstrap-icons/font/bootstrap-icons.css';
+
+import TaskInput from './components/TaskInput';
+import TaskTable from './components/TaskTable';
 
 function App() {
   return (
-    <div className="App">
-      <p>hi</p>
+    <div className='container mt-5'>
+      <div className='card card-body text-center'>
+        <h1>Task List</h1>
+        <hr></hr>
+        <p>Our Task List</p>
+
+        <TaskInput/>
+        <TaskTable/>
+      </div>
     </div>
   );
 }
